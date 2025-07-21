e56fecac42cc3639e89ea4f9284e7fb1bd49e57b
Peterson Kariuki <kariukinjoroge13@gmail.com>
2025-07-21 16:17:14 +0300

    fixed env

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,8 +1,1 @@
-interface ImportMetaEnv {
-  VITE_GEMINI_API_KEY: string;
-  // add other env vars here
-}
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
 /// <reference types="vite/client" />
